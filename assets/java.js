1a96c22e9781de54aedfc67cb24758b8d18ca077
Krystyn Smith <krskrsmith@gmail.com>
2021-08-12 23:40:46 -0500

    i finished the areas that collect user information finally. cleaned up notes as well. began working on for loops

--- assets/java.js
+++ assets/java.js
@@ -6,81 +6,72 @@ var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = "!@#$%^&*.~";
 
-// insert all variables of the password length and complexit
-//var lowerCaseLetters;
-//var upperCaseLetters;
-//var numbers;
-//var specialChar;
 
 
-//clicking the button to promt a window to ask about pw criteria
-
-
+//this function collects all of the user parameters for their password
 function generatePassword() {
   
-
 //collect input for length
-//what happens if they choose under 8 - please type a number between 8-128
-//what happens if they choose over 128 - please type a number between 8-128
-//what happens if they write a word - please type a number between 8-128
-//what happens if they choose a number between 8-128
+var userPwLength = prompt("How long would you like your password to be? (8-128)");
 
-/*
-function (pwLength) {
-  prompt("How long would you like your password to be? (8-128)");
-if ((pwLength < 8) || (pwLength > 128)) {
-  prompt("Enter a number between 8-128.");
-} else if (pwLength = "8 - 128") {
-  alert("Thank you!")
-}
-}
-*/
+if (userPwLength < 8 || userPwLength > 128) {
+  alert("HEY. Type a number between 8-128.");
+} else if  (!userPwLength === true) {
+  alert("YO. Type a number between 8-128.");
+} else (userPwLength = 8 - 128); {
+};
+
 
 //prompt y/n for lower case
-//lower case n
-//lower case y
-function determineLower() {
-    if (confirm("Would you like to use lower case letters?"));{
-        console.log("something happened");
-    }
-} else {console.log("adsf")
-}
+var determineLower = confirm("Would you like to use lower case letters?\nOk = Yes\nCancel = No");
 
+if (determineLower == true) {
+   console.log("AHHHHH") 
+} else {console.log("adsf");
+}
 
 
 //promt y/n for upper case
-//lower case n
-//lower case y
-function determineUpper {
+var determineUpper = confirm("Would you like to use lower case letters?\nOk = Yes\nCancel = No");
 
+if (determineUpper == true) {
+   console.log("AHHHHH") 
+} else {console.log("adsf");
 }
-
 
 
 
 //promt y/n for numbers
-//numbers n
-//numbers y
+var determineNumb = confirm("Would you like to use numbers?\nOk = Yes\nCancel = No");
 
+if (determineNumb == true) {
+   console.log("OHKAY") 
+} else {console.log("duuuude");
+}
 
 
 //prompt y/n for special char
-//special char n
-//special char y
+var determineSpec = confirm("Would you like to use special characters?\nOk = Yes\nCancel = No");
 
+if (determineSpec == true) {
+   console.log("GAAAHHH") 
+} else {console.log("rrhhheeee");
+}
 
+var password = ""
 
 //generate password on screen
-
-
-
-
-
-
 /*
-function lowerCaseLetters() {
+Collected Variables
+userPwLength
+determineLower
+determineUpper
+determineNumb
+determineSpec
+*/
+
   var result = '';
-  var characters = 'abcdefghijklmnopqrstuvwxyz';
+  var characters = userPwLength;
   var charactersLength = characters.length;
   for ( var i = 0; i < length; i++ ) {
     result += characters.charAt(Math.floor(Math.random() * 
@@ -89,8 +80,14 @@ charactersLength));
  return result;
 }
 console.log(makeid(5));
-*/
+
+
+
+return password
 }
+
+
+
 
 // Write password to the #password input
 function writePassword() {
