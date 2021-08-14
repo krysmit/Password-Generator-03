91a62fd5f509e76511370309cc8254c445e6db86
Krystyn Smith <krskrsmith@gmail.com>
2021-08-13 23:25:46 -0500

    corrected the gathering of information and a little bit of the for loop.

--- assets/java.js
+++ assets/java.js
@@ -5,7 +5,7 @@ var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
 var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var numbers = "0123456789";
 var specialChar = "!@#$%^&*.~";
-
+var availableChars = ""
 
 
 //this function collects all of the user parameters for their password
@@ -15,14 +15,16 @@ function generatePassword() {
 var userPwLength = prompt("How long would you like your password to be? (8-128)");
 
 if (userPwLength < 8 || userPwLength > 128) {
-  alert("HEY. Type a number between 8-128.");
+  alert("HEY. Type a number between 8-128.")
+  return;
 } else if  (!userPwLength === true) {
-  alert("YO. Type a number between 8-128.");
+  alert("YO. Type a number between 8-128.")
+  return;
 } else (userPwLength = 8 - 128); {
 };
 
 
-//prompt y/n for lower case
+//y/n for lower case
 var determineLower = confirm("Would you like to use lower case letters?\nOk = Yes\nCancel = No");
 
 if (determineLower == true) {
@@ -31,7 +33,7 @@ if (determineLower == true) {
 }
 
 
-//promt y/n for upper case
+//y/n for upper case
 var determineUpper = confirm("Would you like to use upper case letters?\nOk = Yes\nCancel = No");
 
 if (determineUpper == true) {
@@ -41,7 +43,7 @@ if (determineUpper == true) {
 
 
 
-//promt y/n for numbers
+//y/n for numbers
 var determineNumb = confirm("Would you like to use numbers?\nOk = Yes\nCancel = No");
 
 if (determineNumb == true) {
@@ -50,19 +52,21 @@ if (determineNumb == true) {
 }
 
 
-//prompt y/n for special char
+//y/n for special char
 var determineSpec = confirm("Would you like to use special characters?\nOk = Yes\nCancel = No");
 
 if (determineSpec == true) {
    console.log("special yes") 
 } else {console.log("special no");
-return;
 }
 
+
+
+//Compiling answers into a password
 var password = ""
 
 /*
-Collected Variables
+Collected Variables from above:
 userPwLength
 determineLower
 determineUpper
@@ -70,60 +74,47 @@ determineNumb
 determineSpec
 */
 
+
 if (determineLower == true){
-   password += Math.random(lowerCaseLetters);
-//   availableChars += lowerCaseLetters
+   password += Math.floor(Math.random(lowerCaseLetters))
+   availableChars += determineLower
    console.log("lower case in use");
 } else {console.log("lower case not in use");
 }
 
 if (determineUpper == true){
-   password += Math.random(upperCaseLetters)
-//   availableChars += lower case string
-   console.log("upper case in use")
+   password += Math.floor(Math.random(upperCaseLetters))
+   availableChars += determineUpper
+   console.log("upper case in use");
  } else {console.log("upper case not in use");
 }
 
 if (determineNumb == true){
-   password += Math.random(numbers)
-//   availableChars += lower case string
-   console.log("number in use")
+   password += Math.floor(Math.random(numbers))
+   availableChars += determineNumb
+   console.log("number in use");
  } else {console.log("number not in use");
 }
 
  if (determineSpec == true){
-   password += Math.random(specialChar)
-//   availableChars += lower case string
-   console.log("special char in use")
+   password += Math.floor(Math.random(specialChar))
+   availableChars += determineSpec
+   console.log("special char in use");
  } else {console.log("special not in use");
 }
 
 
-//function() {
- //  Math.floor(Math.random()*userPwLength)
-//}
 
-
-
-
-var result = '';
-
-//var charactersLength = characters.length;
+//Compiling the password
 
 for ( var i = 0; i < userPwLength; i++ ) {
 
-password += Math.random(availableChars);
+// subtract how many you are grabbing to make the password lenght correct???
 
+password += Math.floor(Math.random(availableChars));
 }
 
-
-
-return result;
-
-
-
-
-return password
+return password;
 
 };
 
